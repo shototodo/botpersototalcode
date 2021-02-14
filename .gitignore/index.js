93493ed72e14df8c36149e9b910e7f67c2a92804
shototodo <75580160+shototodo@users.noreply.github.com>
2021-02-14 13:15:44 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,6 +3,7 @@ const prefix = "+";
 const Client = new Discord.Client();
 const bdd = require("./bdd.json");
 const fs = require("fs");
+const moment = require ("moment")
 
 // Commande de statut//
 Client.on("ready", async () => {
@@ -272,6 +273,34 @@ Client.on("message", message => {
 });
 
 // Commande de Mute, unmute & tempmute //
+
+// Commande de Stat user //
+Client.on("message", async message => {
+
+    if (message.author.bot) return;
+
+    if (message.content.startsWith(prefix + "info")) {
+        if (message.mentions.users.first()) {
+            user = message.mentions.users.first();
+        } else {
+            user = message.author;
+        }
+        const member = message.guild.member(user);
+
+        const embed = new Discord.MessageEmbed()
+            .setColor('#ff5555')
+            .setThumbnail(user.avatarURL)
+            .setTitle(`Information sur ${user.username}#${user.discriminator} :`)
+            .addField('ID du compte:', `${user.id}`, true)
+            .addField('Pseudo sur le serveur :', `${member.nickname ? member.nickname : 'Aucun'}`, true)
+            .addField('A crée son compte le :', `${moment.utc(user.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
+            .addField('A rejoint le serveur le :', `${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
+            .addField('Status:', `${user.presence.status}`, true)
+            .addField('Roles :', member.roles.cache.map(roles => `${roles.name}`).join(', '), true)
+            .addField(`En réponse a :`, `${message.author.username}#${message.author.discriminator}`)
+        message.channel.send(embed)
+    }
+})
 
 Client.on("guildCreate", guild => {
     bdd[guild.id] = {}
